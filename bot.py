2b42f6ab9b752a9b40d7127f2d3093b046fd29f7
hyperkekb4-maker <hyper.kekb4@gmail.com>
2025-11-17 00:15:16 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,8 +65,7 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
             [InlineKeyboardButton("USDT-BNB", callback_data="vip_bnb")]
         ]
         await update.message.reply_text(
-               "<blockquote>200$ - 1 Month</blockquote>"
-            "\n\n"
+            "<code>1 Month – 200$</code>\n"
             "After making a deposit, send us the screenshot,\n"
             "and the access link is sent automatically.",
             reply_markup=InlineKeyboardMarkup(keyboard),
@@ -98,9 +97,7 @@ async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
         waiting_for_screenshot[user_id] = "TRC"
         keyboard = [[InlineKeyboardButton("Send Screenshot", callback_data="send_screenshot")]]
         await query.message.reply_text(
-            "<b>After depositing to Wallet, send the screenshot below, usually less than 30 minute is confirmed</b>"
-            "\n\n"
-            "<code>TSxvZs96scypQ2Bc67c4jqN68fdNVCJNKw</code>",
+            "<code>Send your TRC-20 payment screenshot:</code>",
             reply_markup=InlineKeyboardMarkup(keyboard),
             parse_mode="HTML"
         )
@@ -109,13 +106,10 @@ async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
         waiting_for_screenshot[user_id] = "BNB"
         keyboard = [[InlineKeyboardButton("Send Screenshot", callback_data="send_screenshot")]]
         await query.message.reply_text(
-            "<b>After depositing to Wallet, send the screenshot below, usually less than 30 minute is confirmed</b>"
-            "\n\n"
-            "<code>0xa8F380Ef9BC7669418B9a8e4bA38EA2d252d0003</code>",
+            "<code>Send your BNB payment screenshot:</code>",
             reply_markup=InlineKeyboardMarkup(keyboard),
             parse_mode="HTML"
         )
-
 
     elif query.data == "send_screenshot":
         if user_id not in waiting_for_screenshot:
