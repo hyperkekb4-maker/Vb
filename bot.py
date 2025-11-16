ac91e55cb31198066c453fc672c8057f96e2a98c
hyperkekb4-maker <hyper.kekb4@gmail.com>
2025-11-16 14:25:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
             [InlineKeyboardButton("USDT-BNB", callback_data="vip_bnb")]
         ]
         await update.message.reply_text(
-            "<code>1 Month – 200$</code>\n"
+            "1 Month – 200$\n\n"
             "After making a deposit, send us the screenshot,\n"
             "and the access link is sent automatically.",
             reply_markup=InlineKeyboardMarkup(keyboard),
